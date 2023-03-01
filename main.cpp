e8ee414b4d86febb49410dfde6921c87f0c37601
dinosaurich <124108522+dinosaurich@users.noreply.github.com>
2023-03-01 11:03:48 +0300

    Исправил недочеты
    Максимально уменьшил int main(), убрал глобальные переменные

--- main.cpp
+++ main.cpp
@@ -3,24 +3,37 @@
 #include <math.h>
 #include <windows.h>
 using namespace std;
-char array1[10][10];
-int x,y,spxx,spyy,s;
 class wildtiger{
 public:
-int startpos_x,startpos_y;
+int startpos_x,startpos_y,x,y;
 double s;
-void changepos(int spx, int spy){
+char array1[10][10];
+void init(){
+startpos_x=0;
+startpos_y=0;
+s=0;
+for(int i =0;i<10;i++){
+    for(int j=0;j<10;j++){
+        array1[i][j]='0';
+        //cout<<array1[i][j];
+    }
+    //cout<<""<<endl;
+}
+
+
+}
+void changepos(){
 //cout<<"startpos_x:"<<spx<<endl;
 //cout<<"startpos_y:"<<spy<<endl;
 x=rand()%11;
 y=rand()%11;
-array1[spx][spy]='0';
+array1[startpos_x][startpos_y]='0';
 array1[x][y]='*';
-spxx=x;
-spyy=y;
+startpos_x=x;
+startpos_y=y;
 //cout<<"x:"<<x<<endl;
 //cout<<"y"<<y<<endl;
-s=sqrt(pow((spx-x),2)+pow((spy-y),2));
+s=sqrt(pow((startpos_x-x),2)+pow((startpos_y-y),2));
 //cout<<"s:"<<s<<endl;
 x=0;
 y=0;
@@ -37,24 +50,11 @@ s=0;
 
 int main()
 {
-
 wildtiger wt;
-wt.startpos_x=0;
-wt.startpos_y=0;
-wt.s=0;
-for(int i =0;i<10;i++){
-    for(int j=0;j<10;j++){
-        array1[i][j]='0';
-        //cout<<array1[i][j];
-    }
-    //cout<<""<<endl;
-}
+wt.init();
 while(true){
-wt.changepos(wt.startpos_x,wt.startpos_y);
-wt.startpos_x=spxx;
-wt.startpos_y=spyy;
+wt.changepos();
 Sleep(5000);
 }
-
     return 0;
 }
